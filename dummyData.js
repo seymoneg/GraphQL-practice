c3be5ae78c33411828ccb2532010933ccea77722
Seymone Gugneja <seymonegug@gmail.com>
2023-12-28 17:10:07 -0500

    Add files via upload

---
+++ dummyData.js
@@ -0,0 +1,38 @@
+UserList = [
+  {
+    id: 1,
+    name: "Alice",
+    username: "alice",
+    age: 32,
+    nationality: "China",
+  },
+  {
+    id: 2,
+    name: "John",
+    username: "john",
+    age: 44,
+    nationality: "United States of America",
+  },
+  {
+    id: 3,
+    name: "Caroline",
+    username: "caroline",
+    age: 20,
+    nationality: "Poland",
+  },
+  {
+    id: 4,
+    name: "Jake",
+    username: "jake",
+    age: 27,
+    nationality: "Lithuania",
+  },
+  {
+    id: 5,
+    name: "Billy",
+    username: "billy",
+    age: 55,
+    nationality: "Fiji",
+  },
+];
+module.exports = { UserList };
